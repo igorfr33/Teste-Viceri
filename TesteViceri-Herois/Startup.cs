ed928a158c60e97520eb48ad966bdf833ec21996
unknown <igorrabelof10@gmail.com>
2022-04-16 15:57:39 -0300

    Cors Habilitado

--- TesteViceri-Herois/Startup.cs
+++ TesteViceri-Herois/Startup.cs
@@ -4,69 +4,88 @@ using TesteViceri_Herois.Data;
 
 namespace TesteViceri_Herois
 {
-        public class Startup
+    public class Startup
+    {
+        #region Propriedades Públicas
+        public IConfiguration Configuration { get; }
+        #endregion
+
+        #region Construtores
+        public Startup(IConfiguration configuration)
         {
-            #region Propriedades Públicas
-            public IConfiguration Configuration { get; }
-            #endregion
-
-            #region Construtores
-            public Startup(IConfiguration configuration)
-            {
-                Configuration = configuration;
-            }
-            #endregion
-
-            #region Métodos/Operadores Públicos
-            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-            {
-                if (env.IsDevelopment())
-                {
-
-                    app.UseDeveloperExceptionPage();
-                    app.UseSwagger();
-                    app.UseSwaggerUI(opt => { opt.SwaggerEndpoint("/swagger/v1/swagger.json", "TesteViceri-Herois"); });
-                }
-                else
-                {
-                    app.UseExceptionHandler("/Error");
-                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                    app.UseHsts();
-                }
-
-
-                app.UseHttpsRedirection();
-                app.UseStaticFiles();
-
-                app.UseRouting();
-
-                app.UseAuthorization();
-
-                app.UseEndpoints(endpoints =>
-                {
-                    endpoints.MapRazorPages();
-                    endpoints.MapControllers();
-                });
-
-
-
-            }
-            // This method gets called by the runtime. Use this method to add services to the container.
-            public void ConfigureServices(IServiceCollection services)
-            {
-                var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-                var mySqlConnection = Configuration.GetConnectionString("DefaultConnection");
-                services.AddDbContext<ApplicationContext>(options => options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
-
-                services.AddControllers();
-
-                services.AddRazorPages();
-
-                services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "TesteViceri-Herois", Version = "v1", }); });
-
-                services.AddCors();
-            }
-            #endregion
+            Configuration = configuration;
         }
+        #endregion
+
+        #region Métodos/Operadores Públicos
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            #region Configurações do Cors
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+            app.UseCors(MyAllowSpecificOrigins);
+            #endregion
+
+            if (env.IsDevelopment())
+            {
+
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(opt => { opt.SwaggerEndpoint("/swagger/v1/swagger.json", "TesteViceri"); });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+            });
+
+
+
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            #region Habilitar o Cors
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: "_myAllowSpecificOrigins", builder =>
+                {
+                    builder.WithOrigins("http://127.0.0.1:5500");
+                });
+            });
+            #endregion
+
+            #region Conexão com o Banco de Dados
+            var mySqlConnection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationContext>(options => options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+            #endregion
+
+            services.AddControllers();
+
+            services.AddRazorPages();
+
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "TesteViceri", Version = "v1", }); });
+
+            services.AddCors();
+        }
+        #endregion
     }
+}
